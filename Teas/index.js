76065296ef2fb5858b2d5cfb09579b6eac21d758
Bhabin <poon.bhabin6332@gmail.com>
2025-04-22 20:55:19 +0545

    initial commit

---
+++ Teas/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+const PORT = 3000;
+
+let teaList = [];
+let _id = 1;
+
+// add or post the tea
+app.post("/tea", (req, res) => {
+  const teas = req.body;
+  teas._id = _id++;
+  teaList.push(teas);
+
+  res.status(200).json({
+    message: "Succesfully receive data !!",
+    data: teaList,
+  });
+});
+
+// get the colletction tea
+app.get("/tea", (req, res) => {
+  res.status(200).json(teaList);
+});
+
+//get the tea by id
+app.get("/tea/:id", (req, res) => {
+  const _id = parseInt(req.params.id);
+  const tea = teaList.find((tea) => tea._id === _id);
+
+  if (tea) {
+    res.status(200).json({
+      message: "Successfully received tea by using id",
+      data: tea,
+    });
+  } else {
+    res.status(404).json({
+      message: "Tea not found",
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Express Server listen on PORT ${PORT}`);
+});
