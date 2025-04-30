dcd3f099d6fbf9ef857fc17f5df10e02f3878d31
Bhabin <poon.bhabin6332@gmail.com>
2025-04-30 14:39:22 +0545

    add: models for project

---
+++ VidTube/src/models/comment.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+  {
+    content: { type: String, required: true },
+    video: { type: Schema.Types.ObjectId, ref: "Video" },
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  {
+    timeStamp: true,
+  }
+);
+
+commentSchema.plugin(mongooseAggregatePaginate);
+
+export const Comment = mongoose.model("Comment", commentSchema);
