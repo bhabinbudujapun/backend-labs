7e9e7c441187c22b7d9ac21ff80898b186fec087
Bhabin <poon.bhabin6332@gmail.com>
2025-05-01 20:58:31 +0545

    add middleware, controller, routes and also install some packages

--- VidTube/src/app.js
+++ VidTube/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -15,8 +16,13 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
+
+// import routes
+import router from "./routes/user.routes.js";
 
 // routes
 app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/users", userRouter);
 
 export { app };
