f98379004b7fbaf74a01ee7253c4ea6441afb8a8
Bhabin <poon.bhabin6332@gmail.com>
2025-05-12 16:05:27 +0545

    feat(create.book): add book from local(multer) to cloud(cloudinary)

--- eLibrary/src/book/book.controller.js
+++ eLibrary/src/book/book.controller.js
@@ -1,5 +1,9 @@
 import createHttpError from "http-errors";
 import Book from "./book.model.js";
+import cloudinary from "../config/cloudinary.js";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import fs from "node:fs";
 
 // List all books
 const listBooks = async (req, res, next) => {
@@ -28,17 +32,55 @@ const getSingleBook = async (req, res, next) => {
 
 // Create a new book
 const createBook = async (req, res, next) => {
-  const { title, genre } = req.body;
-  const file = req.files;
-  res.json({
-    title,
-    genre,
-    file,
-  });
+  const { title, genre, description } = req.body;
+  const files = req.files;
+
+  const coverImageType = files.coverImage[0].mimetype.split("/").at(-1);
+  const coverImageFileName = files.coverImage[0].filename;
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const coverImageFilePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    coverImageFileName
+  );
+
+  try {
+    const coverImageUpload = await cloudinary.uploader.upload(
+      coverImageFilePath,
+      {
+        filename_override: coverImageFileName,
+        folder: "book-covers",
+        format: coverImageType,
+      }
+    );
+
+    const bookFileType = files.file[0].mimetype.split("/").at(-1);
+    const bookFileName = files.file[0].filename;
+    const _dirname = dirname(fileURLToPath(import.meta.url));
+    const bookFilePath = path.resolve(
+      _dirname,
+      "../../public/data/uploads",
+      bookFileName
+    );
+
+    const bookFileUpload = await cloudinary.uploader.upload(bookFilePath, {
+      resource_type: "raw",
+      filename_override: bookFileName,
+      folder: "book-pdfs",
+      format: bookFileType,
+    });
+
+    console.log(coverImageUpload);
+    console.log(bookFileUpload);
+
+    await fs.promises.unlink(coverImageFilePath);
+    await fs.promises.unlink(bookFilePath);
+
+    res.status(201).json({ message: "Successfully, book added!!" });
+  } catch (error) {
+    console.log(error);
+    return next(createHttpError(500, "Error while uploading the file"));
+  }
 };
 
 export { listBooks, getSingleBook, createBook };
-
-// createBook,
-// updateBook,
-// deleteBook,
