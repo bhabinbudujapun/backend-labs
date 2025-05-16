5f1cbea823ec88f8f37945344545df03d64bc5a3
bhabinpun <poon.bhabin6332@gmail.com>
2025-05-16 22:18:35 +0545

    docs(user.route): add comments for user route for registration and login, each steps explanation

--- eLibrary/src/user/user.route.js
+++ eLibrary/src/user/user.route.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { loginUser, registerUser } from "./user.controller.js";
 
+// Create a new router for user-related routes
 const userRoute = express.Router();
 
+// Route for registering a new user
+// POST /api/users/register
 userRoute.post("/register", registerUser);
 
+// Route for logging in an existing user
+// POST /api/users/login
 userRoute.post("/login", loginUser);
 
+// Export the router to be used in the main app
 export default userRoute;
